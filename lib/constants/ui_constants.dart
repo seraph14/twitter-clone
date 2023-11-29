2837251f1f4070d8eef24e4c9e1432c0a9c4f456
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2023-11-29 12:44:27 +0000

    [UI] home view

--- lib/constants/ui_constants.dart
+++ lib/constants/ui_constants.dart
@@ -15,4 +15,10 @@ class UIConstants {
       centerTitle: true,
     );
   }
+
+  static List<Widget> bottomTabBarPages = [
+    Text('Feed Screen'),
+    Text('Search Screen'),
+    Text('Notification Screen'),
+  ];
 }
--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:twitter_clone/constants/assets_constants.dart';
+import 'package:twitter_clone/constants/ui_constants.dart';
+import 'package:twitter_clone/theme/pallete.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   static route() => MaterialPageRoute(
         builder: ((context) => const HomeView()),
       );
@@ -8,9 +13,56 @@ class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _page = 0;
+  final appBar = UIConstants.appBar();
+
+  void onPageChanged(int index) {
+    setState(() {
+      _page = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('home')),
+    return Scaffold(
+      appBar: appBar,
+      body: IndexedStack(
+        index: _page,
+        children: UIConstants.bottomTabBarPages,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: _page,
+        onTap: onPageChanged,
+        backgroundColor: Pallete.backgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _page == 0
+                  ? AssetsConstants.homeFilledIcon
+                  : AssetsConstants.homeOutlinedIcon,
+              color: Pallete.whiteColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsConstants.searchIcon,
+              color: Pallete.whiteColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _page == 2
+                  ? AssetsConstants.notifFilledIcon
+                  : AssetsConstants.notifOutlinedIcon,
+              color: Pallete.whiteColor,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
